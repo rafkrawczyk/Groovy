02b69a1b02ccf1900620abb86e49be3a70fcf79d
rafkrawczyk <54412881+rafkrawczyk@users.noreply.github.com>
2025-12-09 23:39:43 +0100

    Add files via upload

---
+++ GroovyObject.groovy
@@ -0,0 +1,47 @@
+class Person implements GroovyInterceptable {
+
+    private Map<String, Object> personalCharacteristics = [:]
+
+    @Override
+    void setProperty(String propertyName, Object newValue) {
+        System.out.println ">> INTERCEPTED: Setting property '$propertyName' to '$newValue'"
+        personalCharacteristics[propertyName] = newValue
+    }
+
+    @Override
+    Object getProperty(String propertyName) {
+        System.out.println ">> INTERCEPTED: Getting property '$propertyName'"
+        if (propertyName == 'personalCharacteristics') return personalCharacteristics
+        return personalCharacteristics.containsKey(propertyName) ?
+                personalCharacteristics[propertyName] : "N/A"
+    }
+
+    @Override
+    Object invokeMethod(String methodName, Object args) {
+        System.out.println ">> INTERCEPTED: Calling method '$methodName' with args '$args'"
+
+        if (methodName == "speak") {
+            return "Hello!"
+        }
+
+        if (methodName == "add") {
+            def list = args as List
+            return list.sum()
+        }
+
+        return "Method '$methodName' handled generically"
+    }
+}
+
+def mark = new Person()
+
+mark.hairColor = "Blonde"
+mark.height = 190
+
+println "Hero: ${mark.hairColor}"
+println "Code: ${mark.height}"
+println "Missing Prop: ${mark.eyeColor}"
+
+println "Result 1: " + mark.speak()
+println "Result 2: " + mark.add(10, 20, 30)
+println "Result 3: " + mark.travel()
